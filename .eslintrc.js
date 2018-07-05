219e754694010375eab07b0c8d13412e0382642e
Sergey Dmitriev <Sergey_Dmitriev@epam.com>
2018-07-05 13:12:31 +0300

    Adds eslint settings

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+    extends: ['eslint:recommended', 'plugin:react/recommended'],
+    parser: "babel-eslint",
+    parserOptions: {
+        "ecmaFeatures": {
+            "jsx": true,
+        }
+    }
+};
