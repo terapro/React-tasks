020cab4c9630ef970f2d9245efdbea208ac26934
Sergey Dmitriev <Sergey_Dmitriev@epam.com>
2018-07-10 13:10:02 +0300

    Adds sorting by the date or by the rating

--- src/components/results/index.js
+++ src/components/results/index.js
@@ -7,27 +7,54 @@ import {Item} from './item/item.js';
 import {Summary} from '../summary/index.js'
 
 
+
+
 class Results extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      status: 'noResults', // 'noResults'
       sortItems: {
         list: ['release date', 'rating'],
         active: 'release date'
-      }
+      },
+
     };
-    this.changeSortItemClick = (el) => {
+    this.changeSortItemClick = (el) => { // CallBack for changing this.state (it changes active genre)
       this.setState({sortItems: {
           list: this.state.sortItems.list,
           active: el.target.id
         }})
-    }
+    };
+    this.sortResult = (res, parameter) => { // Function for sorting results
+      const sortParametersMatching = {
+        'release date': 'release_date',
+        'rating': 'vote_average'
+      };
 
+      parameter = sortParametersMatching[parameter];
+
+      switch (parameter) {
+        case 'release_date' : {
+          return res.sort(sortFuncDates);
+        }
+        case 'vote_average': {
+          return res.sort(sortFuncRating);
+        }
+      }
+
+      function sortFuncRating (a,b) {
+        return +b[parameter] - +a[parameter]
+      }
+      function sortFuncDates(a,b) {
+        return Date.parse(b[parameter]) - Date.parse(a[parameter]);
+      }
+    }
   }
 
   render() {
-    const {searchResult} = this.props;
+    let {searchResult} = this.props;
+    searchResult = this.sortResult(searchResult, this.state.sortItems.active); // Sort result here
+
 
     if (searchResult.length) { // When at least 1 film to show
       return (
