dbbe54988c0b13b083d359c628ac551dd888d205
Sergey Dmitriev <Sergey_Dmitriev@epam.com>
2018-07-23 12:13:04 +0300

    Code refactoring in the src/actions.js

--- src/actions.js
+++ src/actions.js
@@ -1,64 +1,52 @@
 import C from 'src/constants';
 
-export const startSearch = (phrase, type) => {
-  return {
-    type: C.START_SEARCH,
-    payload: {
+export const startSearch = (phrase, type) => (
+  {type: C.START_SEARCH,
+   payload: {
       searchPhrase: phrase,
       searchType: type,
       replaceCurrentResultsBy: [],
       loadingData: true
     }
-  }
-};
+  });
 
-export const resultsToStore = (results) => {
-  return {
-    type: C.RESULTS_TO_STORE,
+export const resultsToStore = results =>
+  ({type: C.RESULTS_TO_STORE,
     payload: {
       foundResults: results,
       loadingData: false
     }
-  }
-};
+  });
 
-export const openFilm = (film) => {
-  const genre = film['genres'][0];
-  return {
-    type: C.OPEN_FILM,
-    payload: {
+export const openFilm = film =>
+  ({type: C.OPEN_FILM,
+   payload: {
       filmToOpen: {...film},
-      filmGenre: genre,
+      filmGenre: film['genres'][0],
       loadingData: true,
       searchType: 'genres'
     }
-  }
-};
+  });
 
-export const recommendedToStore = (results) => {
-  return {
+export const recommendedToStore = results => ({
     type: C.RECOMMENDED_TO_STORE,
     payload: {
       foundRecommended: results,
       loadingData: false
     }
-  }
-};
+  });
 
-export const openSearch = () => {
-  return {
-    type: C.OPEN_SEARCH,
+export const openSearch = () =>
+  ({type: C.OPEN_SEARCH,
     payload: {}
-  }
-};
+  });
 
-export const changeItem = (newCriterion) => {
-  return {
-    type: C.CHANGE_SORTING,
+export const changeItem = newCriterion =>
+  ({type: C.CHANGE_SORTING,
     payload: {
       chosenParameter: newCriterion
     }
-  }
-};
+  });
+
 
 
